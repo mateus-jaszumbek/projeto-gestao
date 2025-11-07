fbf7ad343210c6a17dce10faf6eb9baf78ca3485
mateus.jaszumbek <mateus.jaszumbek.01tec@gmail.com>
2025-11-06 21:41:00 -0300

    teste

--- utils/auth.ts
+++
@@ -1,3 +0,0 @@
-export const saveToken = (token: string) => localStorage.setItem("token", token);
-export const getToken = () => (typeof window !== "undefined" ? localStorage.getItem("token") : null);
-export const logout = () => localStorage.removeItem("token");
